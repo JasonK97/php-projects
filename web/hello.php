f0daa698903c3e4a99797aa8cf70f7d5e74348f8
Jason Kent <jasonbkent@gmail.com>
2020-01-09 16:40:12 -0700

    added hello world page

--- web/hello.php
+++ web/hello.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Assignment 1</title>
+    <style>
+    body {
+        background-color: grey;
+    }
+    </style>
 </head>
 <body>
     <?php 
