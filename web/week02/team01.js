3d955dbeb911b3a10e060a8873616bcc8e97dc78
Jason Kent <jasonbkent@gmail.com>
2020-01-14 16:47:00 -0700

    enter

---
+++ web/week02/team01.js
@@ -0,0 +1,5 @@
+function buttonClicked() {
+    document.getElementById("div1").innerHTML = "CLICKED";
+    document.getElementById("div2").visibility = "hidden";
+    document.getElementById("div3").visibility = "hidden";
+}
---
+++ web/week02/teamActivity1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="team01.css">
+    <script href="team01.js"></script>
+    <title>Team Activity Week 2</title>
+</head>
+<body>
+    <div id="div1">Here is some text</div>
+    <div id="div2">Some more text for your viewing pleasure</div>
+    <div id="div3">LAst text for you to see bb</div>
+    <button type="button">Click Me!</button>
+</body>
+</html>
