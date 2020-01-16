ddd04438c0d8927b6e9d5e1371e124aec41a9e88
Jason Kent <jasonbkent@gmail.com>
2020-01-16 12:24:13 -0700

    enter

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assign.css">
+  <title>Jason Kent | Assignments</title>
+</head>
+<body>
+  <div id="block">
+  <?php echo "<h1 id=\"title\">Coming Soon</h1>"?>
+  </div>
+</body>
+</html>
