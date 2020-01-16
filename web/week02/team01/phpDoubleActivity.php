2fc96a074d19b67e08b14866457e50c2e29dba17
Jason Kent <jasonbkent@gmail.com>
2020-01-16 16:12:30 -0700

    enter

---
+++ web/week02/team01/phpDoubleActivity.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="teamstuff.css">
+  <title>PHP Team Activity</title>
+</head>
+<body>
+<h1>PHP Team Activity</h1>
+ <div style="width:50%; float: left;">
+    <?php include 'phpTeamAct.php'; ?>
+ </div>
+ <div style="width:50%; float: right;">
+    <?php include 'phpTeamAct.php'; ?>
+ </div>
+</body>
+</html>
