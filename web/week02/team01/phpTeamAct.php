62be13c1d55884cf3cde9ff825bf7de6d7fbe863
Jason Kent <jasonbkent@gmail.com>
2020-01-16 15:57:16 -0700

    enter

--- web/week02/team01/phpTeamAct.php
+++ web/week02/team01/phpTeamAct.php
@@ -9,9 +9,10 @@
 </head>
 <body>
   <?php
-  for ($i = 0; $i < 10; $i++) {
-      echo "<div>This is div #" . $i . "</div>";
-  }
-  ?>
+  for ($i = 0; $i < 10; $i++)  { ?>
+      <div id="<?php echo $i; ?>" class="numberedDivs"
+      <?php if($i % 2 == 0){echo "style='color:red;'";} ?>>
+      This is div #<?php echo $i; ?></div>
+  <?php } ?>
 </body>
 </html>
