2b4a105cf33c10a15ef575820ef9bf6022fe2cd3
Jason Kent <jasonbkent@gmail.com>
2020-01-21 15:38:10 -0700

    enter

---
+++ web/week03/week03-post.php
@@ -0,0 +1,14 @@
+<?php
+    $email = htmlspecialchars($_POST["emailForPHP"]);
+    $password = htmlspecialchars($_POST["passwordForPHP"]);
+
+    echo "Here is your email $email, and your password is $password";
+?>
+<!--<html>
+    <body>
+        <button>Ready to Checkout?</button>
+    </body>
+</html>
+<form>
+
+</form> -->
