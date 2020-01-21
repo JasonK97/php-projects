09ce60716c4652aedf8f93f1878d95728a94ad74
Jason Kent <jasonbkent@gmail.com>
2020-01-21 16:25:35 -0700

    enter

--- web/week03/display.php
+++ web/week03/display.php
@@ -19,8 +19,8 @@
          echo "Comments: $comment<br><br>";
 
          echo "Visited continents: <br>";
-         if (!empty($continents)){
-            foreach($continents as $selected){
+         if (!empty($continent)){
+            foreach($continent as $selected){
                echo $continents[$selected] . "<br>";
             }
          }
