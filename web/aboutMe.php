fb52a1058d4dd7fab4c52e4178fce1904bda59e3
Jason Kent <jasonbkent@gmail.com>
2020-01-21 13:29:05 -0700

    enter

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="shortcut icon" type="image/x-icon" href="jayswanme.png">
     <meta charset="UTF-8">
@@ -9,18 +10,20 @@
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
     <title>Jason Kent | About Me</title>
 </head>
+
 <body>
     <div id="block">
         <h1 id="title">About Me</h1>
         <img src="MayceeAndMe.jpg" alt="A picture of me">
         <p class="info">
-            My name is Jason Kent, I'm a Software Engineering Major, I'm a Junior, and I'm getting 
-            married in April! I love playing Guitar and Soccer, and I enjoy playing videogames with 
-            my friends in my free time (Currently Siege and MW mostly). I'm from just north of Seattle, 
+            My name is Jason Kent, I'm a Software Engineering Major, I'm a Junior, and I'm getting
+            married in April! I love playing Guitar and Soccer, and I enjoy playing videogames with
+            my friends in my free time (Currently Siege and MW mostly). I'm from just north of Seattle,
             Washington, born and raised. I like the snow, but I hate the wind, so Rexburg is a win/lose.
         </p><br><br>
         <a href="landingPage.php"><strong>Back to Landing Page</strong></a>
         <?php echo "<p id='date'>Today is " . date("m/d/Y") . "</p>"; ?>
     </div>
 </body>
+
 </html>
