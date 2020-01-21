2d515d20eecc6935369673d3e719a70d389449a6
Jason Kent <jasonbkent@gmail.com>
2020-01-21 15:51:48 -0700

    enter

--- web/personalProject/DDHomepage.php
+++ web/personalProject/DDHomepage.php
@@ -5,14 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="assign.css">
+  <link rel="stylesheet" href="../assign.css">
   <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
   <title>Jason Kent | Assignments</title>
 </head>
 <body>
   <div id="block">
   <?php echo "<h1 id=\"title\">Coming Soon</h1>" ?>
-  <a href="landingPage.php">Back to Landing Page</a>
+  <a href="../landingPage.php">Back to Landing Page</a>
   </div>
 </body>
 </html>
