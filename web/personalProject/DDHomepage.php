bd1b4b05375c718648e1736778bf2012184b0171
Jason Kent <jasonbkent@gmail.com>
2020-01-21 15:49:53 -0700

    enter

---
+++ web/personalProject/DDHomepage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" type="image/x-icon" href="jayswanme.png">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assign.css">
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
+  <title>Jason Kent | Assignments</title>
+</head>
+<body>
+  <div id="block">
+  <?php echo "<h1 id=\"title\">Coming Soon</h1>" ?>
+  <a href="landingPage.php">Back to Landing Page</a>
+  </div>
+</body>
+</html>
