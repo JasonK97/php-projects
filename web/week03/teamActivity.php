78c806bc8094e7fbcaefecf83c96a21e5efca01e
Jason Kent <jasonbkent@gmail.com>
2020-01-21 15:59:46 -0700

    enter

---
+++ web/week03/teamActivity.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Activity Week03</title>
+</head>
+<body>
+    <form>
+        Name:<br>
+        <input type="text"><br>
+        Email:<br>
+        <input name="emailForPHP" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        Major:<br>
+        <input type="radio">Computer Science
+    </form>
+</body>
+</html>
