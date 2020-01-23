388b00d09732775e387326275324d5045702b86e
Jason Kent <jasonbkent@gmail.com>
2020-01-23 11:38:20 -0700

    Updated from VSCode

--- web/week03/assignment03/browse.php
+++ web/week03/assignment03/browse.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="shop.css">
-    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC|Cabin&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cabin|ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
     <title>Kent Emporium | Browse</title>
 </head>
 
