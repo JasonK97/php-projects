37061a6af770dac04235bf4a754d626883984f77
Jason Kent <jasonbkent@gmail.com>
2020-01-23 16:04:46 -0700

    VSCode

--- web/week03/thursday/thursdayClassTemp/thursdaySession2.php
+++ web/week03/thursday/thursdayClassTemp/thursdaySession2.php
@@ -1,6 +1,9 @@
 <?php 
    // start session
+   session_start();
    // save session variables into local variables
+   $c = $_SESSION["favcolor"];
+   $a = $_SESSION["favanimal"];
 ?>
-<h1><?php // use the session variables ?></h1>
+<h1>Your favorite color is <?=$c?> and your favorite animal in <?=$a?></h1>
 <?php ?>
