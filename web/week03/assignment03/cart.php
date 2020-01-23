db56b5b8e1b34defe01a811a25e71a7ac59e2904
Jason Kent <jasonbkent@gmail.com>
2020-01-23 16:25:22 -0700

    VSCode

---
+++ web/week03/assignment03/cart.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="shop.css">
+    <link href="https://fonts.googleapis.com/css?family=Cabin|ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
+    <title>Kent Emporium | Your Cart</title>
+</head>
+
+<body>
+    <div class="header">
+        <a href="browse.php" class="logo">Kent Emporium</a>
+        <div class="header-right">
+            <a href="browse.php">Home</a>
+            <a class="active" href="cart.php">View Cart</a>
+        </div>
+    </div>
+</body>
+</html>
