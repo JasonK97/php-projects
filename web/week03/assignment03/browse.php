ac18928a95ef8f37fcfab14430bafb7694b71d16
Jason Kent <jasonbkent@gmail.com>
2020-01-23 11:21:54 -0700

    enter

--- web/week03/assignment03/browse.php
+++ web/week03/assignment03/browse.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="shop.css">
+    <title>Kent Emporium | Browse</title>
 </head>
 <body>
-    
+    <div id="header">
+        <h1 id="title">Kent Emporium</h1>
+        <input type="text">
+        <button type="button">Go</button>
+    </div>
 </body>
 </html>
