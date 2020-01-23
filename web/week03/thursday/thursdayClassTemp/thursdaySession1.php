ecc33976c9672aaf62119b0c26fcda946c864f14
Jason Kent <jasonbkent@gmail.com>
2020-01-23 16:01:13 -0700

    VSCode Update

--- web/week03/thursday/thursdayClassTemp/thursdaySession1.php
+++ web/week03/thursday/thursdayClassTemp/thursdaySession1.php
@@ -1,13 +1,20 @@
 <?php
    // Start the session
+   session_start();
 ?>
 <!DOCTYPE html>
 <html>
    <body>
       <?php
          // remove previous session variable
+
          // Set session variables
+         $_SESSION["favcolor"] = "teal";
+         $_SESSION["favanimal"] = "cat";
+         $_SESSION["favgame"];
          // echo that variables have been set
+         echo "Your favorite color is " . $_SESSION["favcolor"] . "<br>";
+         echo "Your favorite animal is " . $_SESSION["favanimal"];
 ?>
       <a href="thursdaySession2.php">Check the variables on another page</a>
 
