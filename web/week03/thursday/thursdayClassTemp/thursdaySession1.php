e55db1c2d40e8e30d6346ee288bfd84f5701bc43
Jason Kent <jasonbkent@gmail.com>
2020-01-23 18:09:05 -0700

    Updated from VSCode

--- web/week03/thursday/thursdayClassTemp/thursdaySession1.php
+++ web/week03/thursday/thursdayClassTemp/thursdaySession1.php
@@ -12,6 +12,7 @@
          $_SESSION["favcolor"] = "teal";
          $_SESSION["favanimal"] = "cat";
          $_SESSION["pen"];
+         $_SESSION["pencil"];
          // echo that variables have been set
          echo "Your favorite color is " . $_SESSION["favcolor"] . "<br>";
          echo "Your favorite animal is " . $_SESSION["favanimal"] . "<br>";
@@ -21,17 +22,17 @@
       <h3>Just for kicks, let's try it with a form</h3>
       <form action="thursdaySession2.php" method="POST">
          Pen
-         <input type="submit" name="SubmitPen" value="Submit">
+         <input type="submit" name="SubmitPen" value="Submit"><br>
          Pencil
          <input type="submit" name="SubmitPencil" value="Submit">
       </form>
       <?php 
          if(isset($_POST['SubmitPen'])) {
-            $_SESSION["pen"] = $_POST["pen"];
+            $_SESSION["pen"] = "Pen";
          }
 
          if(isset($_POST['SubmitPencil'])) {
-            $_SESSION["pencil"] = $_POST["pencil"];
+            $_SESSION["pencil"] = "Pencil";
          }
       ?>
    </body>
--- web/week03/thursday/thursdayClassTemp/thursdaySession2.php
+++ web/week03/thursday/thursdayClassTemp/thursdaySession2.php
@@ -7,7 +7,12 @@
 ?>
 <h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?></h1>
 
-<?php if(isset($_SESSION["pen"])) { ?>
    <h3>Again, for kicks... from a form</h3><br>
-   <?php echo "You selected the " . $_SESSION["pen"]?>
-<?php } ?>
+   <?php 
+      if(isset($_SESSION["pen"])) {
+         echo "You selected the " . $_SESSION["pen"] . "<br>";
+      }
+      else if (isset($_SESSION["pencil"])) {
+         echo "You selected the " . $_SESSION["pencil"] . "<br>";
+      }
+   ?>
