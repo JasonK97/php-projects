4165d24c87b421d13ed32cef56f28c41882595a9
Jason Kent <jasonbkent@gmail.com>
2020-01-31 14:13:09 -0700

    Updated from VSCode

---
+++ web/week04/Prove/create.sql
@@ -0,0 +1,30 @@
+CREATE TABLE public.user
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    display_name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE public.character
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES public.user(id),
+    user_display_name VARCHAR(100) NOT NULL REFERENCES public.user(display_name),
+    character_name VARCHAR(100) NOT NULL,
+    class VARCHAR(100) NOT NULL,
+    character_level INT NOT NULL,
+    race VARCHAR(100) NOT NULL,
+    alignment VARCHAR(100) NOT NULL,
+    strength INT NOT NULL,
+	dexterity   INT NOT NULL,
+	constitution INT NOT NULL,
+	intelligence INT NOT NULL,
+	wisdom INT NOT NULL,
+	charisma INT NOT NULL,
+	maxHP INT NOT NULL,
+	accessible_items VARCHAR(1000), 
+	currency INT,
+	feats VARCHAR(1000) NOT NULL,
+	features VARCHAR(1000) NOT NULL
+);
