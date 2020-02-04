154354abcdbdbd6b327d0ad6bcc5ff4dbb67df60
Jason Kent <jasonbkent@gmail.com>
2020-02-04 16:10:45 -0700

    Tuesday Stuff

--- web/week05/temp/index.php
+++ web/week05/temp/index.php
@@ -8,12 +8,12 @@
     while($fRow = $family_mambers->fetch(PDO::FETCH_ASSOC)) {
         $first_name = $fRow["first_name"];
         $last_name = $fRow["last_name"];
-        $relationship = $fRow["relationship"];
+        $relationship_id = $fRow["relationship"];
 
-        $relationshipName = $db->prepare("SELECT description FROM week05rel WHERE id = $relationship");
+        $relationshipName = $db->prepare("SELECT description FROM week05rel WHERE id = $relationship_id");
         $relationshipName->execute();
         while ($rRow = $relationshipName->fetch(PDO::FETCH_ASSOC)) {
-            $relationshipName = $rRow["description"];
+            $relationshipName2 = $rRow["description"];
         }
         echo "<p>$first_name $last_name is my $relationshipName</p>";
     }
