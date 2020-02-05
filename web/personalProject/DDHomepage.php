360cde7dbadff939433a4d87742a7bc1612c39b1
Jason Kent <jasonbkent@gmail.com>
2020-02-05 16:45:43 -0700

    Updated from VSCode

--- web/personalProject/DDHomepage.php
+++ web/personalProject/DDHomepage.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../assign.css">
+  <link rel="stylesheet" href="DDCC.css">
   <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
   <title>Kent Creator | Homepage</title>
 </head>
@@ -13,9 +13,9 @@
   <div id="block">
   <h1>Welcome to Jason's D&D Character Creator</h1>
   <?php 
-      // ini_set('display_errors', 1);
-      // ini_set('display_startup_errors', 1);
-      // error_reporting(E_ALL);
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
       
       
       require "dbConnect.php";
