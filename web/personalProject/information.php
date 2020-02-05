d695383e57ddbf8ce468f0f9045afe287c8d1039
Jason Kent <jasonbkent@gmail.com>
2020-02-05 16:48:30 -0700

    Updated from VSCode

--- web/personalProject/information.php
+++ web/personalProject/information.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../assign.css">
+  <link rel="stylesheet" href="DDCC.css">
   <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
   <title>Kent Creator | Inventory</title>
 </head>
