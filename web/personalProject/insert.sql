09a1cb349a4b54716fda9487ca4ee1ed7f629ec7
Jason Kent <jasonbkent@gmail.com>
2020-02-07 13:44:18 -0700

    Updated from VSCode

--- web/personalProject/insert.sql
+++ web/personalProject/insert.sql
@@ -41,6 +41,46 @@ INSERT INTO about (user_id,
                 'Armor Proficiency (Chainmail)',
                 'Male, Tall, Thin, Strong');
 
+INSERT INTO character (user_id,
+                    character_name,
+                    class,
+                    character_level,
+                    race,
+                    alignment,
+                    strength,
+                    dexterity,
+                    constitution,
+                    intelligence,
+                    wisdom,
+                    charisma,
+                    maxHP)
+            VALUES ('1',
+                    'Jaskier',
+                    'Bard',
+                    '1',
+                    'Human',
+                    'Unaligned',
+                    '16',
+                    '14',
+                    '13',
+                    '11',
+                    '10',
+                    '12',
+                    '15');
+
+INSERT INTO about (user_id,
+                   character_id,
+                   accessible_items,
+                   currency,
+                   feats,
+                   features)
+        VALUES ('1',
+                '2',
+                'Lute, Rapier, Boots of Swiftness',
+                '150',
+                'Armor Proficiency (Leather)',
+                'Male, Average, Prideful Aura');
+
 INSERT INTO profile (username, password, display_name)
 VALUES ('MikeD184', 'jasonkent', 'Michael Dufour');
 
@@ -78,7 +118,7 @@ INSERT INTO about (user_id,
                    feats,
                    features)
         VALUES ('2',
-                '2',
+                '3',
                 'Bow, Arrows (100), Armor',
                 '150',
                 'Armor Proficiency (Leather)',
