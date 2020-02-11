9dfe043b96b4e299160859c20b02fa5bec8b0fee
Jason Kent <jasonbkent@gmail.com>
2020-02-11 16:37:45 -0700

    Team Act

--- web/week06/teamAct/team.php
+++ web/week06/teamAct/team.php
@@ -1,48 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php
-	require("dbConnect.php");
-	$db = get_db();
-?>
+
 <body>
-<div class="container" style="margin-top:50px;">
-      <form action="insert.php" method="POST">
-         <div class="form-row">
+
+    <?php
+        require("dbConnect.php");
+        $db = get_db();
+    ?>
+
+    <form action="insert.php" method="POST">
+        <div class="form-row">
             <div>
-               <input type="text" placeholder="Book" name="book">
+                <input type="text" placeholder="Book" name="book">
             </div>
             <div>
-               <input type="text" placeholder="Chapter" name="chapter">
+                <input type="text" placeholder="Chapter" name="chapter">
             </div>
             <div>
-               <input type="text" placeholder="Verses" name="verse">
+                <input type="text" placeholder="Verses" name="verse">
             </div>
             <div>
-               <input type="textarea" placeholder="Chapter" name="chapter">
+                <input type="textarea" placeholder="Chapter" name="chapter">
             </div>
             <div>
-                  <select id="inputFood" class="form-control" name="topic">
-                     <?php
-                        $statement = $db->prepare("SELECT * FROM topics");
-                        $statement->execute();
-                        while($tRow = $statement->fetch(PDO::FETCH_ASSOC)) {
-                           $id = $tRow['id'];
-                           $topic = $tRow['name'];
-                           echo "<option value='$id'>$topic</option>";
-                        }
-                     ?>
-                  </select>
-               </div>
-               <div>
-                  <button type="submit">Save this scripture</button>
-               </div>
-         </div>
-      </form>
-   </div>
+                <select id="inputFood" class="form-control" name="topic">
+                    <?php
+                    $statement = $db->prepare("SELECT * FROM topics");
+                    $statement->execute();
+                    while ($tRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+                        $id = $tRow['id'];
+                        $topic = $tRow['name'];
+                        echo "<option value='$id'>$topic</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div>
+                <button type="submit">Save this scripture</button>
+            </div>
+        </div>
+    </form>
 </body>
+
 </html>
