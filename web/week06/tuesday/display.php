25c2d68e48201c455874559fd91861d5fb5c694a
Jason Kent <jasonbkent@gmail.com>
2020-02-11 15:45:08 -0700

    Tuesday work

--- web/week06/tuesday/display.php
+++ web/week06/tuesday/display.php
@@ -5,6 +5,22 @@
 	<body>
 		<div class="container">
          <?php
+            $personId = $_GET['personId'];
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
+            $statement->bindValue(':personId', $personId);
+            $statement->execute();
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+               $id = $row['id'];
+               $first = $row['first_name'];
+               $last = $row['last_name'];
+               $food_id = $row['food_type'];
+
+               $statement = $db->prepare('SELECT food FROM w6_food WHERE ID = :food_id');
+               $statement->execute();
+               while ($fRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+                  $food = $fRow['food'];
+               }
+               echo "<h1>$first $last's favorite food is $food<h1>";            }
             // retrieve url parameter
             // execute query to pull up data from that id
             // execute another query to get food data
