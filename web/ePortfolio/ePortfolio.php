2f403c831c15cc64630ee6ddb452c3fd920047de
Jason Kent <jasonbkent@gmail.com>
2020-02-12 14:17:00 -0700

    Creating ePortfolio

---
+++ web/ePortfolio/ePortfolio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="shortcut icon" type="image/x-icon" href="../jayswanme.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="port.css">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
+    <title>Jason Kent | ePortfolio</title>
+</head>
+
+<body>
+    <div id="block">
+        <h1 id="title">Jason Kent | Software Engineering Student</h1>
+        <br>
+        <img src="code.jpg" alt="some code">
+        <br>
+        <h2 class="info">Click here to view my Resume: <br><a href="resume.html">My Resume</a></h2>
+        <br><br><br>
+        <h2 class="info">Click here for my Github: <br><a href="https://github.com/JasonK97">Github</a></h2>
+        <br><br><br>
+        <h2 class="info">Click here for my Personal Project: <br><a href="personalProject/DDHomepage.php">D&D Character Creator</a></h2>
+        <?php echo "<p id='date'>Today is " . date("m/d/Y") . "</p>"; ?>
+    </div>
+</body>
+
+</html>
