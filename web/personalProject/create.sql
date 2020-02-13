6e4ddcd38a4d303fe8b67254010f44fa08065b2b
Jason Kent <jasonbkent@gmail.com>
2020-02-13 16:29:11 -0700

    Updates tables

--- web/personalProject/create.sql
+++ web/personalProject/create.sql
@@ -1,4 +1,12 @@
-CREATE TABLE public.profile
+DROP TABLE IF EXISTS alignment;
+DROP TABLE IF EXISTS about;
+DROP TABLE IF EXISTS stats;
+DROP TABLE IF EXISTS class;
+DROP TABLE IF EXISTS race;
+DROP TABLE IF EXISTS character;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile
 (
     id                  SERIAL          NOT NULL    PRIMARY KEY,
     username            VARCHAR(100)    NOT NULL    UNIQUE,
@@ -6,20 +14,38 @@ CREATE TABLE public.profile
     display_name        VARCHAR(100)    NOT NULL    UNIQUE
 );
 
-CREATE TABLE public.character
+CREATE TABLE class
 (
-    id                  	SERIAL          NOT NULL    PRIMARY KEY,
-    user_id             	INT	            NOT NULL    REFERENCES public.profile(id),
-    character_name     		VARCHAR(100)    NOT NULL,
-    class               	VARCHAR(100)    NOT NULL,
-    character_level     	INT             NOT NULL,
-    race                	VARCHAR(100)    NOT NULL
+    id			SERIAL			NOT NULL	PRIMARY KEY,
+    class       VARCHAR(100)    NOT NULL
 );
 
-CREATE TABLE public.stats
+CREATE TABLE race
+(
+    id			SERIAL			NOT NULL	PRIMARY KEY,
+    race        VARCHAR(100)    NOT NULL
+);
+
+CREATE TABLE alignment
+(
+    id              SERIAL          NOT NULL    PRIMARY KEY,
+    alignment       VARCHAR(100)    NOT NULL
+);
+
+CREATE TABLE character
+(
+    id                  	SERIAL          NOT NULL    PRIMARY KEY,
+    user_id             	INT	            NOT NULL    REFERENCES profile(id),
+    class_id               	INT             NOT NULL    REFERENCES class(id),
+    race_id                	INT             NOT NULL    REFERENCES race(id),
+    character_name     		VARCHAR(100)    NOT NULL,
+    character_level     	INT             NOT NULL
+);
+
+CREATE TABLE stats
 (
     id						SERIAL			NOT NULL	PRIMARY KEY,
-	character_id			INT				NOT NULL	REFERENCES public.character(id),
+	character_id			INT				NOT NULL	REFERENCES character(id),
     maxHP               	INT             NOT NULL,
     strength            	INT             NOT NULL,
 	dexterity           	INT             NOT NULL,
@@ -29,49 +55,52 @@ CREATE TABLE public.stats
 	charisma            	INT             NOT NULL
 );
 
-CREATE TABLE public.about
+CREATE TABLE about
 (
 	id						SERIAL			NOT NULL	PRIMARY KEY,
-	character_id			INT				NOT NULL	REFERENCES public.character(id),
+	character_id			INT				NOT NULL	REFERENCES character(id),
+    alignment_id           	INT             NOT NULL    REFERENCES alignment(id),
 	accessible_items    	VARCHAR(1000), 
 	currency            	INT,
 	feats               	VARCHAR(1000)   NOT NULL,
-	features            	VARCHAR(1000)   NOT NULL,
-    alignment           	VARCHAR(100)    NOT NULL
+	features            	VARCHAR(1000)   NOT NULL
 );
 
-INSERT INTO character (race) VALUES ('Dragonborn');
-INSERT INTO character (race) VALUES ('Dwarf');
-INSERT INTO character (race) VALUES ('Elf');
-INSERT INTO character (race) VALUES ('Human');
-INSERT INTO character (race) VALUES ('Gnome');
-INSERT INTO character (race) VALUES ('Half-Elf');
-INSERT INTO character (race) VALUES ('Half-Orc');
-INSERT INTO character (race) VALUES ('Halfling');
-INSERT INTO character (race) VALUES ('Tiefling');
+INSERT INTO race (race) VALUES ('Dragonborn');
+INSERT INTO race (race) VALUES ('Dwarf');
+INSERT INTO race (race) VALUES ('Elf');
+INSERT INTO race (race) VALUES ('Human');
+INSERT INTO race (race) VALUES ('Gnome');
+INSERT INTO race (race) VALUES ('Half-Elf');
+INSERT INTO race (race) VALUES ('Half-Orc');
+INSERT INTO race (race) VALUES ('Halfling');
+INSERT INTO race (race) VALUES ('Tiefling');
 
 
-INSERT INTO character (class) VALUES ('Barbarian');
-INSERT INTO character (class) VALUES ('Bard');
-INSERT INTO character (class) VALUES ('Cleric');
-INSERT INTO character (class) VALUES ('Druid');
-INSERT INTO character (class) VALUES ('Fighter');
-INSERT INTO character (class) VALUES ('Monk');
-INSERT INTO character (class) VALUES ('Paladin');
-INSERT INTO character (class) VALUES ('Ranger');
-INSERT INTO character (class) VALUES ('Rogue');
-INSERT INTO character (class) VALUES ('Sorcerer');
-INSERT INTO character (class) VALUES ('Warlock');
-INSERT INTO character (class) VALUES ('Wizard');
-INSERT INTO character (class) VALUES ('--Artificer--');
-INSERT INTO character (class) VALUES ('--Blood Hunter--');
+INSERT INTO class (class) VALUES ('Barbarian');
+INSERT INTO class (class) VALUES ('Bard');
+INSERT INTO class (class) VALUES ('Cleric');
+INSERT INTO class (class) VALUES ('Druid');
+INSERT INTO class (class) VALUES ('Fighter');
+INSERT INTO class (class) VALUES ('Monk');
+INSERT INTO class (class) VALUES ('Paladin');
+INSERT INTO class (class) VALUES ('Ranger');
+INSERT INTO class (class) VALUES ('Rogue');
+INSERT INTO class (class) VALUES ('Sorcerer');
+INSERT INTO class (class) VALUES ('Warlock');
+INSERT INTO class (class) VALUES ('Wizard');
+INSERT INTO class (class) VALUES ('--Artificer--');
+INSERT INTO class (class) VALUES ('--Blood Hunter--');
 
-INSERT INTO about (alignment) VALUES ('Lawful Good');
-INSERT INTO about (alignment) VALUES ('Neutral Good');
-INSERT INTO about (alignment) VALUES ('Chaotic Good');
-INSERT INTO about (alignment) VALUES ('Lawful Neutral');
-INSERT INTO about (alignment) VALUES ('Neutral');
-INSERT INTO about (alignment) VALUES ('Chaotic Neutral');
-INSERT INTO about (alignment) VALUES ('Lawful Evil');
-INSERT INTO about (alignment) VALUES ('Neutral Evil');
-INSERT INTO about (alignment) VALUES ('Chaotic Evil');
+INSERT INTO alignment (alignment) VALUES ('Lawful Good');
+INSERT INTO alignment (alignment) VALUES ('Neutral Good');
+INSERT INTO alignment (alignment) VALUES ('Chaotic Good');
+INSERT INTO alignment (alignment) VALUES ('Lawful Neutral');
+INSERT INTO alignment (alignment) VALUES ('Neutral');
+INSERT INTO alignment (alignment) VALUES ('Chaotic Neutral');
+INSERT INTO alignment (alignment) VALUES ('Lawful Evil');
+INSERT INTO alignment (alignment) VALUES ('Neutral Evil');
+INSERT INTO alignment (alignment) VALUES ('Chaotic Evil');
+
+INSERT INTO profile (username, password, display_name)
+VALUES ('JasonK97', 'jasonkent', 'Jason Kent');
