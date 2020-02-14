486a8b1a93972cba237b2e1f054ad242f9045955
Jason Kent <jasonbkent@gmail.com>
2020-02-14 11:29:46 -0700

    testing

--- web/personalProject/insert.php
+++ web/personalProject/insert.php
@@ -41,18 +41,18 @@ try
 	$aStatement->bindValue(':alignment', $alignment);
     $aStatement->execute();
 
-    $cQuery = 'INSERT INTO character (/*user_id, class_id, race_id,*/ character_name, character_level) VALUES (/*:user_id, :class_id, :race_id,*/ :name, :level)';
+    $cQuery = 'INSERT INTO character (user_id, class_id, race_id, character_name, character_level) VALUES (:user_id, :class_id, :race_id, :name, :level)';
     $cStatement = $db->prepare($cQuery);
-    // $cStatement->bindValue(':user_id', $user_id);
-    // $cStatement->bindValue(':class_id', $class_id);
-    // $cStatement->bindValue(':race_id', $race_id);
+    $cStatement->bindValue(':user_id', $db->lastInsertId("profile_id_seq"));
+    $cStatement->bindValue(':class_id', $db->lastInsertId("class_id_seq"));
+    $cStatement->bindValue(':race_id', $db->lastInsertId("race_id_seq"));
 	$cStatement->bindValue(':name', $name);
 	$cStatement->bindValue(':level', $level);
     $cStatement->execute();
     
-    $sQuery = 'INSERT INTO stats (/*character_id,*/ maxHP, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES (/*:character_id,*/ :maxHP, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)';
+    $sQuery = 'INSERT INTO stats (character_id, maxHP, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES (:character_id, :maxHP, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)';
     $sStatement = $db->prepare($sQuery);
-   // $sStatement->bindValue(':character_id', $character_id);
+    $sStatement->bindValue(':character_id', $db->lastInsertId("character_id_seq"));
 	$sStatement->bindValue(':maxHP', $maxHP);
     $sStatement->bindValue(':strength', $strength);
     $sStatement->bindValue(':dexterity', $dexterity);
@@ -62,9 +62,9 @@ try
     $sStatement->bindValue(':charisma', $charisma);
     $sStatement->execute();
     
-    $abQuery = 'INSERT INTO about (/*character_id,*/ accessible_items, currency, feats, features) VALUES (/*:character_id,*/ :accessible_items, :currency, :feats, :features)';
+    $abQuery = 'INSERT INTO about (character_id, accessible_items, currency, feats, features) VALUES (:character_id, :accessible_items, :currency, :feats, :features)';
     $abStatement = $db->prepare($abQuery);
-   // $sStatement->bindValue(':character_id', $character_id);
+    $abStatement->bindValue(':character_id', $db->lastInsertId("character_id_seq"));
 	$abStatement->bindValue(':accessible_items', $accessible_items);
     $abStatement->bindValue(':currency', $currency);
     $abStatement->bindValue(':feats', $feats);
@@ -74,14 +74,14 @@ try
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
 
-	$charId = $db->lastInsertId("character_id_seq");
+	//$charId = $db->lastInsertId("character_id_seq");
 }
 catch (Exception $ex)
 {
 	echo "Error with DB. Details: $ex";
 	die();
 }
-header("Location: displayCharacter.php/?profileId=$charId");
+header("Location: displayCharacter.php/?profileId=" . ':character_id');
 
 die(); 
 ?>
