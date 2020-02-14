96637986a61e98ae2dbddf3ae732baa4c73a4735
Jason Kent <jasonbkent@gmail.com>
2020-02-14 10:18:31 -0700

    testing

--- web/personalProject/insert.php
+++ web/personalProject/insert.php
@@ -41,18 +41,18 @@ try
 	$aStatement->bindValue(':alignment', $alignment);
     $aStatement->execute();
 
-    $cQuery = 'INSERT INTO character (user_id, class_id, race_id, character_name, character_level) VALUES (:user_id, :class_id, :race_id, :name, :level)';
+    $cQuery = 'INSERT INTO character (/*user_id, class_id, race_id,*/ character_name, character_level) VALUES (:user_id, :class_id, :race_id, :name, :level)';
     $cStatement = $db->prepare($cQuery);
-    $cStatement->bindValue(':user_id', $user_id);
-    $cStatement->bindValue(':class_id', $class_id);
-    $cStatement->bindValue(':race_id', $race_id);
+    // $cStatement->bindValue(':user_id', $user_id);
+    // $cStatement->bindValue(':class_id', $class_id);
+    // $cStatement->bindValue(':race_id', $race_id);
 	$cStatement->bindValue(':name', $name);
 	$cStatement->bindValue(':level', $level);
     $cStatement->execute();
     
-    $sQuery = 'INSERT INTO stats (character_id, maxHP, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES (:character_id, :maxHP, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)';
+    $sQuery = 'INSERT INTO stats (/*character_id,*/ maxHP, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES (:character_id, :maxHP, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)';
     $sStatement = $db->prepare($sQuery);
-    $sStatement->bindValue(':character_id', $character_id);
+   // $sStatement->bindValue(':character_id', $character_id);
 	$sStatement->bindValue(':maxHP', $maxHP);
     $sStatement->bindValue(':strength', $strength);
     $sStatement->bindValue(':dexterity', $dexterity);
@@ -62,9 +62,9 @@ try
     $sStatement->bindValue(':charisma', $charisma);
     $sStatement->execute();
     
-    $abQuery = 'INSERT INTO about (character_id, accessible_items, currency, feats, features) VALUES (:character_id, :accessible_items, :currency, :feats, :features)';
+    $abQuery = 'INSERT INTO about (/*character_id,*/ accessible_items, currency, feats, features) VALUES (:character_id, :accessible_items, :currency, :feats, :features)';
     $abStatement = $db->prepare($abQuery);
-    $sStatement->bindValue(':character_id', $character_id);
+   // $sStatement->bindValue(':character_id', $character_id);
 	$abStatement->bindValue(':accessible_items', $accessible_items);
     $abStatement->bindValue(':currency', $currency);
     $abStatement->bindValue(':feats', $feats);
