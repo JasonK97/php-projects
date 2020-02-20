567c245d0ca2e1d222f4c6aa29481304f6ab03c4
Jason Kent <jasonbkent@gmail.com>
2020-02-20 15:41:52 -0700

    testing

--- web/personalProject/insert.php
+++ web/personalProject/insert.php
@@ -43,14 +43,18 @@ try
 	// $aStatement->bindValue('$alignment', $alignment);
     // $aStatement->execute();
 
-    $cQuery = "INSERT INTO character (user_id, class_id, race_id, character_name, character_level) VALUES ('$user_id', '$class_id', '$race_id', '$name', '$level')";
+    $user_id = 1;
+    $class_id = 1;
+    $race_id = 1;
+
+    $cQuery = 'INSERT INTO character (user_id, class_id, race_id, character_name, character_level) VALUES (:user_id, :class_id, :race_id, :name, :level)';
     $cStatement = $db->prepare($cQuery);
-    // $cStatement->bindValue('$user_id', $db->lastInsertId("profile_id_seq"));
-    // $cStatement->bindValue('$class_id', $db->lastInsertId("class_id_seq"));
-    // $cStatement->bindValue('$race_id', $db->lastInsertId("race_id_seq"));
-	// $cStatement->bindValue('$name', $name);
-	// $cStatement->bindValue('$level', $level);
-    // $cStatement->execute();
+    $cStatement->bindValue(':user_id', $user_id);
+    $cStatement->bindValue(':class_id', $class_id);
+    $cStatement->bindValue(':race_id', $race_id);
+	$cStatement->bindValue(':name', $name);
+	$cStatement->bindValue(':level', $level);
+    $cStatement->execute();
     
     $sQuery = "INSERT INTO stats (character_id, maxHP, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES ('$character_id', '$maxHP', '$strength', '$dexterity', '$constitution', '$intelligence', '$wisdom', '$charisma')";
     $sStatement = $db->prepare($sQuery);
@@ -59,25 +63,27 @@ try
     // $sStatement->bindValue('$strength', $strength);
     // $sStatement->bindValue('$dexterity', $dexterity);
     // $sStatement->bindValue('$constitution', $constitution);
-    // $sStatement->bindValue('$intelligence', $intelligence);
-    // $sStatement->bindValue('$wisdom', $wisdom);
-    // $sStatement->bindValue('$charisma', $charisma);
+    // $sStatement->bindValue(':intelligence', $intelligence);
+    // $sStatement->bindValue(':wisdom', $wisdom);
+    // $sStatement->bindValue(':charisma', $charisma);
     // $sStatement->execute();
     
     $abQuery = "INSERT INTO about (character_id, alignment_id, accessible_items, currency, feats, features) VALUES ('$character_id', '$alignment_id', '$accessible_items', '$currency', '$feats', '$features')";
     $abStatement = $db->prepare($abQuery);
-    // $abStatement->bindValue('$character_id', $db->lastInsertId("character_id_seq"));
-    // $abStatement->bindValue('$alignment_id', $db->lastInsertId("alignment_id_seq"));
-	// $abStatement->bindValue('$accessible_items', $accessible_items);
-    // $abStatement->bindValue('$currency', $currency);
-    // $abStatement->bindValue('$feats', $feats);
-    // $abStatement->bindValue('$features', $features);
+    // $abStatement->bindValue(':character_id', $db->lastInsertId("character_id_seq"));
+    // $abStatement->bindValue(':alignment_id', $db->lastInsertId("alignment_id_seq"));
+	// $abStatement->bindValue(':accessible_items', $accessible_items);
+    // $abStatement->bindValue(':currency', $currency);
+    // $abStatement->bindValue(':feats', $feats);
+    // $abStatement->bindValue(':features', $features);
 	// $abStatement->execute();
 
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
 
     //$charId = $db->lastInsertId("character_id_seq");
+
+    $charId = "SELECT id FROM character ORDER BY id DESC LIMIT 1";
 
 
     echo "$name is a level $level $alignment $race.<br>
@@ -90,7 +96,7 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
-//header("Location: displayCharacter.php/?characterId=$charId");
+header("Location: displayCharacter.php/?characterId=$charId");
 
 die(); 
 ?>
