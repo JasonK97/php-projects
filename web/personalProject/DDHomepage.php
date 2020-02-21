54c6daf7b802931223ae523d6e6572e34ba447c3
Jason Kent <jasonbkent@gmail.com>
2020-02-21 15:15:43 -0700

    CSS connection issue test

--- web/personalProject/DDHomepage.php
+++ web/personalProject/DDHomepage.php
@@ -20,7 +20,7 @@ else
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="DDCC.css">
+  <link rel="stylesheet" href="../personalProject/DDCC.css">
   <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
   <title>Kent Creator | Homepage</title>
 </head>
