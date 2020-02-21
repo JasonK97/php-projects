21b9b958d7efd930fe7b8441dd4629ab1671ac91
Jason Kent <jasonbkent@gmail.com>
2020-02-21 13:42:51 -0700

    Login css connection

--- web/personalProject/Login/signUp.php
+++ web/personalProject/Login/signUp.php
@@ -8,13 +8,13 @@ error_reporting(E_ALL);
 <html lang="en">
 
 <head>
-    <link rel="shortcut icon" type="image/x-icon" href="../jayswanme.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../jayswanme.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="DDCC.css">
+    <link rel="stylesheet" href="../DDCC.css">
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
-    <title>Kent Creator | Homepage</title>
+    <title>Kent Creator | Sign Up</title>
 </head>
 
 <body>
