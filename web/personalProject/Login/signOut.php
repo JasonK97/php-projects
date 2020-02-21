6be65242b3a4469d6e47bb03e3cc0abcfbbb4a6c
Jason Kent <jasonbkent@gmail.com>
2020-02-21 13:12:51 -0700

    Testing Login

--- web/personalProject/Login/signOut.php
+++ web/personalProject/Login/signOut.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require("password.php"); // used for password hashing.
 session_start();
 unset($_SESSION['username']);
