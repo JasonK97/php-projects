21b1e9297d0f997f2519e9151b6bc76a22b154ff
Jason Kent <jasonbkent@gmail.com>
2020-02-21 12:47:37 -0700

    Testing display

--- web/personalProject/displayCharacter.php
+++ web/personalProject/displayCharacter.php
@@ -46,8 +46,8 @@
             $class = $classRow['class'];
          }
 
-         $statement2 = $db->prepare('SELECT * FROM stats WHERE character_id = :characterNumId');
-         $statement2->bindValue(':characterNumId', $characterId);
+         $statement2 = $db->prepare('SELECT * FROM stats WHERE character_id = :characterId');
+         $statement2->bindValue(':characterId', $characterId);
          $statement2->execute();
          while ($sRow = $statement2->fetch(PDO::FETCH_ASSOC)) {
             //$maxNumHP = $sRow['maxHP'];
@@ -59,23 +59,23 @@
             $charismaNum = $sRow['charisma'];
          }
 
-         $statement3 = $db->prepare('SELECT * FROM about WHERE id = :characterId');
-         $statement3->bindValue(':characterId', $characterId);
-         $statement3->execute();
-         while ($aRow = $statement3->fetch(PDO::FETCH_ASSOC)) {
-            $alignment = $aRow['alignment_id'];
-            $accessible_items = $aRow['accessible_items'];
-            $currency = $aRow['currency'];
-            $feats = $aRow['feats'];
-            $features = $aRow['features'];
-         }
+         // $statement3 = $db->prepare('SELECT * FROM about WHERE id = :characterId');
+         // $statement3->bindValue(':characterId', $characterId);
+         // $statement3->execute();
+         // while ($aRow = $statement3->fetch(PDO::FETCH_ASSOC)) {
+         //    $alignment = $aRow['alignment_id'];
+         //    $accessible_items = $aRow['accessible_items'];
+         //    $currency = $aRow['currency'];
+         //    $feats = $aRow['feats'];
+         //    $features = $aRow['features'];
+         // }
 
-         $alignmentName = $db->prepare('SELECT alignment FROM alignment WHERE id = :alignmentId');
-         $alignmentName->bindValue(':alignmentId', $alignment);
-         $alignmentName->execute();
-         while ($aRow = $alignmentName->fetch(PDO::FETCH_ASSOC)) {
-            $alignmentNames = $aRow["alignment"];
-         }
+         // $alignmentName = $db->prepare('SELECT alignment FROM alignment WHERE id = :alignmentId');
+         // $alignmentName->bindValue(':alignmentId', $alignment);
+         // $alignmentName->execute();
+         // while ($aRow = $alignmentName->fetch(PDO::FETCH_ASSOC)) {
+         //    $alignmentNames = $aRow["alignment"];
+         // }
       }
       ?>
 
