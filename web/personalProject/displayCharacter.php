1686b6c5fcf1237952d6357b484e643e5e86bd1a
Jason Kent <jasonbkent@gmail.com>
2020-02-21 15:27:53 -0700

    CSS connection fixes...

--- web/personalProject/displayCharacter.php
+++ web/personalProject/displayCharacter.php
@@ -6,9 +6,123 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" type="text/css" href="DDCC.css">
+   <!--<link rel="stylesheet" type="text/css" href="DDCC.css">-->
    <link href="https://fonts.googleapis.com/css?family=Titillium+Web|Zilla+Slab&display=swap" rel="stylesheet">
    <title>Kent Creator | Character Information Sheet</title>
+   <style>
+      body {
+    background-color: rgb(65, 0, 0);
+}
+
+#block {
+    background-color: rgb(99, 99, 99);
+    border-color: black;
+    padding: 20px;
+    margin-left: 150px;
+    margin-right: 150px;
+    margin-top: 50px;
+}
+
+#title {
+    text-align: center;
+    font-family: 'Titillium Web', sans-serif;
+    font-weight: bold;
+}
+
+h2 {
+    font-family: 'Titillium Web', sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+}
+
+#createCh {
+    text-align: center;
+}
+
+.info {
+    font-family: 'Zilla Slab', serif;
+    font-size: 15px;
+}
+
+.about {
+    font-family: 'Zilla Slab', serif;
+    font-size: 15px;
+}
+
+a {
+    text-decoration: none;
+    color: rgb(97, 0, 0);
+    font-family: 'Titillium Web', sans-serif;
+    font-weight: bold;
+}
+
+a:hover {
+    color: rgb(0, 0, 0);
+}
+
+textarea {
+    resize: none;   
+}
+
+#statNums {
+    list-style-type:none
+}
+
+#blockLogin {
+    background-color: rgb(99, 99, 99);
+    border-color: black;
+    padding: 20px;
+    margin-left: 150px;
+    margin-right: 150px;
+    margin-top: 50px;
+    /* text-align: center; */
+}
+
+#mainForm {
+    text-align: center;
+}
+
+@media only screen and (max-width: 1000px) {
+    body {
+      background-color: rgb(1, 9, 56);
+    }
+
+    #block {
+        background-color: rgb(99, 99, 99);
+        border-color: black;
+        padding: 10px;
+        margin-left: 20px;
+        margin-right: 20px;
+        margin-top: 10px;
+    }
+
+    #blockLogin {
+        background-color: rgb(99, 99, 99);
+        border-color: black;
+        padding: 10px;
+        margin-left: 20px;
+        margin-right: 20px;
+        margin-top: 10px;
+        text-align: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: rgb(8, 0, 85);
+        font-family: 'Titillium Web', sans-serif;
+        font-weight: bold;
+    }
+    
+    a:hover {
+        color: rgb(0, 0, 0);
+    }
+
+    textarea {
+        resize: none;
+        width: 100%;
+    }
+  }
+   </style>
 </head>
 
 <body>
