2f7fbd9af8859dd30a3eebcc4a2e4a1a40d7e8b2
Jason Kent <jasonbkent@gmail.com>
2020-03-02 12:05:32 -0700

    Node.js files

--- web/week08/prove/provew08.js
+++ web/week08/prove/provew08.js
@@ -4,12 +4,19 @@ const http = require('http');
 function onRequest(req, res){
     switch(req.url) {
         case '/home':
+            res.writeHead(404, {"Content-Type": "text/html"});
             res.end('<h1>Welcome to the Homepage!</h1>');
             break;
         case '/getData':
-            res.end('<h1>Get Data</h1>');
+            res.writeHead(200, {"Content-Type": "application/json"});
+            var info = {
+                name: 'Jason Kent',
+                class: 'CS313'
+            }
+            res.end(JSON.stringify(info));
             break;
         default:
+            res.writeHead(404, {"Content-Type": "text/html"});
             res.end('<h1>Error 404: Page not found.</h1>');
             break;
     }
